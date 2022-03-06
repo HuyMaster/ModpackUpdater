874dd5b6b6aef0ac870555d97cc2785121e8950e
HuyMaster <huymastertx88@gmail.com>
2022-03-06 21:50:02 +0700

    program use java 1.8

--- PJ02/src/root/Main.java
+++ PJ02/src/root/Main.java
@@ -13,7 +13,9 @@ public class Main {
 		if (Integer.parseInt(temp.substring(0, temp.indexOf(dot))) >= 11) {
 			fileReader.Main();
 		} else {
-			System.err.println("Require java version 11 or above");
+			System.out.println();
+			System.err.println("Require JRE(JDK) 11 or above");
+			System.out.println();
 		}
 		
 		
