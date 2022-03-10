c60ebbc2e3e825d79d0b0a4b48ea023e3873017c
HuyMaster <huymastertx88@gmail.com>
2022-03-10 17:19:07 +0700

    Check MC is running or not

--- PJ02/src/root/Main.java
+++ PJ02/src/root/Main.java
@@ -3,6 +3,8 @@ package root;
 import root.machine.fileAction;
 import root.machine.fileReader;
 import root.utilities.java;
+import root.utilities.processChecker;
+import root.utilities.sleep;
 
 public class Main {
 
@@ -10,6 +12,11 @@ public class Main {
 		java java = new java();
 		String temp = java.checkVer();
 		char dot = '.';
+		if (processChecker.start("javaw.exe", "Minecraft 1.7.10") == true) {
+			System.out.println("Yeu cau tat Minecraft truoc khi cai dat");
+			sleep.sleep(3);
+			System.exit(1);
+		}
 		if (Integer.parseInt(temp.substring(0, temp.indexOf(dot))) >= 11) {
 			fileReader.Main();
 		} else {
@@ -17,7 +24,7 @@ public class Main {
 			System.err.println("Require JRE(JDK) 11 or above");
 			System.out.println();
 		}
-		
+		sleep.sleep(5);
 		
 	}
 
